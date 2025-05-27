f2ab1db053f9a3ae505fd9fc75a8e9ddddf8a5a0
Robmargar <roberto.mar.garc@gmail.com>
2025-05-26 23:08:47 -0600

    Se practica la validacion de un formulario y regresa los errores  al frontend

--- aprendiendo_PHP/PHP_profundidad/4_validacion/index.php
+++ aprendiendo_PHP/PHP_profundidad/4_validacion/index.php
@@ -9,30 +9,39 @@
 
 <body>
     <h1>Validar formulario en PHP </h1>
+
+    <?php
+    if (isset($_GET['error'])) {
+        $error = $_GET['error'];
+        if ($error == 'faltan_valores') {
+            echo '<h1 style="color:red">Algunos datos del formulario no fueron llenados correctamente </h1>';
+        }
+    }
+    ?>
     <form method="POST" action="procesar_formulario.php">
         <div>
             <label for="name">Nombre: </label>
-            <input type="text" name="name" required="required" pattern="^[a-zA-Záéíóú]+$">
+            <input type="text" name="name" required="required">
         </div>
         <div>
             <label for="apellidop">Apellido Paterno: </label>
-            <input type="text" name="apellidop" required="required" pattern="^[a-zA-Záéíóú]+$">
+            <input type="text" name="apellidop" required="required" >
         </div>
         <div>
             <label for="apellidom">Apellido Materno: </label>
-            <input type="text" name="apellidom" required="required" pattern="^[a-zA-Záéíóú]+$">
+            <input type="text" name="apellidom" required="required" >
         </div>
         <div>
             <label for="edad">Edad: </label>
-            <input type="number" name="edad" required="required" pattern="^[0-9]+$">
+            <input type="text" name="edad" required="required">
         </div>
         <div>
             <label for="email">Email: </label>
-            <input type="text" name="email" required="required" pattern="/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/">
+            <input type="text" name="email" required="required">
         </div>
         <div>
             <label for="pass">Password: </label>
-            <input type="pasword" name="pass" required="required">
+            <input type="password" name="pass" required="required">
         </div>
 
         <input type="submit" value="Enviar">
--- aprendiendo_PHP/PHP_profundidad/4_validacion/procesar_formulario.php
+++ aprendiendo_PHP/PHP_profundidad/4_validacion/procesar_formulario.php
@@ -1,3 +1,83 @@
 <?php
 
+/* Ahora diferencia entre isset y empty
+
+isset= Revisa unicamente si la variable existe, sin importar si traé contenido o no.
+empty= revisa si existe la variable pero tambien si viene vacia por lo tanto auqnue llegue la variable
+        si su contenido es null o vacio la toma como que no a llegado la variable
+
+Por lo tanto en esta ocasión utilizaremos empty para poder validar que el formulario no venga vacio en el caso 
+de que el campo required falle.
+*/
+
+$errores = [];
+
+function validaNombre($nombre)
+{
+    global $errores;
+    if (preg_match("/[0-9]/", $nombre)) {
+        array_push($errores, "Nombre o Apellidos no deben contener números ");
+    };
+};
+
+function validaEdad($edad)
+{
+    global $errores;
+    if (preg_match("/[a-zA-ZáéíóúÁÉÍÓÚ]/", $edad)) {
+        array_push($errores, "Edad no es un número");
+    };
+};
+
+function validaEmail($email)
+{
+    (is_int($email)) ? filter_var($email, FILTER_VALIDATE_EMAIL) : array_push($errores, "Email no tiene un formato valido");
+};
+
+(empty($_POST['name'] == false)) ?
+    validaNombre($_POST['name']) :
+    array_push($errores, 'Nombre vacio');
+
+(empty($_POST['apellidop'] == false)) ?
+    validaNombre($_POST['apellidop']) :
+    array_push($errores, 'Apellido Paterno vacio');
+
+(empty($_POST['apellidom'] == false)) ?
+    validaNombre($_POST['apellidom']) :
+    array_push($errores, 'Apellido Materno vacio');
+
+(empty($_POST['edad'] == false)) ?
+    validaEdad($_POST['edad']) :
+    array_push($errores, 'Edad vacia');
+
+
+var_dump($errores);
+// if (
+//     // !empty($_POST['edad'])
+//     // ||
+//     // !empty($_POST['email'])
+//     // ||
+//     // !empty($_POST['pass'])
+//     ) {
+//     $apellidop = $_POST['apellidop'];
+//     $apellidom = $_POST['apellidom'];
+//     $edad = $_POST['edad'];
+//     $email = $_POST['email'];
+//     $pass = $_POST['pass'];
+// } else {
+//     header("Location:index.php?error=$errores");
+// };
+
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<header>
+    <meta charset="uft-8">
+    <title> Recepcion de formulario</title>
+</header>
+
+<body>
+
+</body>
+
+</html>
